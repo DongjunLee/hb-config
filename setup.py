5aa3119eb488ab2ed8df2cbf231022f3ff0db9ce
DongjunLee <humanbrain.djlee@gmail.com>
2017-09-08 15:51:02 +0900

    chore: update version 0.1.1 setting for pypi dist uploads

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
+import codecs
+import os.path
+
 from setuptools import setup, find_packages
 
-HB_CONFIG_VERSION = '0.1.0'
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
+
+HB_CONFIG_VERSION = '0.1.1'
 
 setup (
     name='hb-config',
     version=HB_CONFIG_VERSION,
     author='Dongjun Lee',
-    packages=find_packages(),
+    url='https://github.com/DongjunLee/hb-config',
     author_email='humanbrain.djlee@gmail.com',
     description='easy to configure your python packge',
-    license='OPEN SOURCE'
+    ong_description=read(fpath('README.md')),
+    keywords = ['config', 'util'],
+    license='MIT license',
+    packages=find_packages(),
+    install_requires=[
+        'pyyaml'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
